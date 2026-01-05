5bd411d928ca090535583af4e3dd39628f9b17fc
hogwartsacademy3 <hogwartsacademy57@gmail.com>
2026-01-05 17:49:33 +0700

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,17 @@
-console.log("auth loaded");
+function login() {
+  const u = document.getElementById("user").value;
+  const p = document.getElementById("pass").value;
+
+  if (u === "student" && p === "hogwarts") {
+    sessionStorage.setItem("login", "yes");
+    window.location.href = "index.html";
+  } else {
+    document.getElementById("msg").innerText = "Access Denied";
+  }
+}
+
+function protect() {
+  if (sessionStorage.getItem("login") !== "yes") {
+    window.location.href = "login.html";
+  }
+}
